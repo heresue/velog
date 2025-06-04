25a20e18925ec8bc77a23578800337823d8b3348
heresue <152346573+heresue@users.noreply.github.com>
2025-06-04 14:18:50 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -11,16 +11,12 @@ repo_path = '.'
 # 'velog-posts' 폴더 경로
 posts_dir = os.path.join(repo_path, 'velog-posts')
 
-# 'velog-posts' 폴더가 없으면 생성
+# 'velog-posts' 폴더가 없다면 생성
 if not os.path.exists(posts_dir):
     os.makedirs(posts_dir)
 
 # 레포지토리 로드
 repo = git.Repo(repo_path)
-
-# Git 커밋 사용자 정보 설정
-repo.git.config('user.name', 'github-actions[bot]')
-repo.git.config('user.email', 'github-actions[bot]@users.noreply.github.com')
 
 # RSS 피드 파싱
 feed = feedparser.parse(rss_url)
@@ -29,17 +25,20 @@ feed = feedparser.parse(rss_url)
 for entry in feed.entries:
     # 파일 이름에서 유효하지 않은 문자 제거 또는 대체
     file_name = entry.title
-    file_name = file_name.replace('/', '-') # 슬래시는 하위 폴더로 인식되므로 대시로 대체
-    file_name = file_name.replace('\\', '') # 역슬래시는 파일 이름에 사용할 수 없으므로 제거
-    file_name += '.md' # 마크다운 파일 확장자 추가
-    file_path = os.path.join(posts_dir, file_name) # 파일 경로
+    file_name = file_name.replace('/', '-')  # 슬래시를 대시로 대체
+    file_name = file_name.replace('\\', '-')  # 백슬래시를 대시로 대체
+    # 필요에 따라 추가 문자 대체
+    file_name += '.md'
+    file_path = os.path.join(posts_dir, file_name)
 
+    # 파일이 이미 존재하지 않으면 생성
     if not os.path.exists(file_path):
         with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(entry.description) # 글 내용을 파일에 쓰기
-        repo.git.add(file_path) # 깃에 파일 추가
-        repo.git.commit('-m', f'Add post: {entry.title}') # 커밋
-  
-# 푸시
+            file.write(entry.description)  # 글 내용을 파일에 작성
 
+        # 깃허브 커밋
+        repo.git.add(file_path)
+        repo.git.commit('-m', f'Add post: {entry.title}')
+
+# 변경 사항을 깃허브에 푸시
 repo.git.push()
